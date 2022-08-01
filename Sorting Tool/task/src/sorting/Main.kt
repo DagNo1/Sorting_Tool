747623fb3654d56b5ffc4070353655d8b9ccd7f6
Dagim Tesfaye <dagcodes@gmail.com>
2022-08-01 13:21:25 +0300

    Stage 2 complete. Now my program can also read inputs in the form of lines, words, and integers.

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -1,14 +1,49 @@
 package sorting
 
+import jdk.jfr.Percentage
 import java.util.Scanner
+import kotlin.system.exitProcess
 
-fun main() {
-    val scanner  = Scanner(System.`in`)
+val scanner  = Scanner(System.`in`)
+
+fun main(args: Array<String>) {
+    val dataType = when {
+        args.size == 2 && args[0] == "-dataType" && args[1] == "long" -> long()
+        args.size == 2 && args[0] == "-dataType" && args[1] == "line" -> line()
+        args.size == 2 && args[0] == "-dataType" && args[1] == "word" -> word()
+        else -> word()
+    }
+}
+fun long() {
     val numbers = mutableListOf<Int>()
-    while(scanner.hasNext()) numbers.add(scanner.nextInt())
+    while (scanner.hasNext()) numbers.add(scanner.nextInt())
     val max: Int = numbers.maxOrNull() ?: 0
     var noOfTimes = 0
     for (i in numbers) if (i == max) noOfTimes++
-    println("Total numbers: ${numbers.size}.")
-    println("The greatest number: $max ($noOfTimes time(s)).")
+    result("numbers", "greatest number",numbers.size, max.toString(), noOfTimes)
+}
+fun line() {
+    val lines = mutableListOf<String>()
+    var max = ""
+    var noOfTimes = 0
+    while (scanner.hasNextLine()) lines.add(scanner.nextLine())
+    lines.forEach { if(it.length > max.length) max = it }
+    lines.forEach { if (it == max) noOfTimes++ }
+    result("lines", "longest line",lines.size, max, noOfTimes)
+}
+fun word() {
+    val words = mutableListOf<String>()
+    while (scanner.hasNext()) words.add(scanner.next())
+    var max = ""
+    var noOfTimes = 0
+    words.forEach { if(it.length > max.length) max = it }
+    words.forEach { if (it == max) noOfTimes++ }
+    result("words", "longest word",words.size, max, noOfTimes)
+}
+fun result(total: String, longest: String, size: Int, max: String, noOfTimes: Int) {
+    val percentage: Double = noOfTimes.toDouble() / size * 100.0
+    val newL = if (total == "lines") "\n" else " "
+    println("Total $total: $size.")
+    println("The $longest:$newL$max$newL($noOfTimes time(s),${percentage.toInt()}%).")
+    exitProcess(0)
 }
