f5b71f4fe67ab64342af5437d39fed8f9b3b118c
Dagim Tesfaye <dagcodes@gmail.com>
2022-08-02 20:10:29 +0300

    Stage 4 complete. Now it can sort through all data types.

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -1,57 +1,62 @@
 package sorting
 
 import java.util.Scanner
-import kotlin.system.exitProcess
+import kotlin.math.roundToInt
 
 val scanner  = Scanner(System.`in`)
 
 fun main(args: Array<String>) {
-    if (args.size == 1 && !args.contains("-sortIntegers")) word()
+    if (args.size == 1 && !args.contains("-sortingType")) word()
     when {
-        args.contains("-sortIntegers") -> sort()
-        args[0] == "-dataType" && args[1] == "long" -> long()
-        args[0] == "-dataType" && args[1] == "line" -> line()
-        args[0] == "-dataType" && args[1] == "word" -> word()
+        args.contains("byCount")  -> when {
+            args.contains("long") -> long("byCount")
+            args.contains("word") -> word("byCount")
+            args.contains("line") -> line("byCount")
+        }
+        args.contains("natural") || (!args.contains("natural") && !args.contains("byCount")) -> when {
+            args.contains("long") -> long("natural")
+            args.contains("word") -> word("natural")
+            args.contains("line") -> line("natural")
+        }
         else -> word()
     }
 }
-fun sort() {
-    val numbers = mutableListOf<Int>()
+fun long(sortType: String = "natural") {
+    var numbers = mutableListOf<Int>()
     while (scanner.hasNext()) numbers.add(scanner.nextInt())
-    val sortedNumbers = numbers.sorted().joinToString(" ")
-    println("Total numbers: ${numbers.size}.")
-    println("Sorted data: $sortedNumbers")
+    val numberString = mutableListOf<String>()
+    numbers = numbers.sorted().toMutableList()
+    numbers.forEach { numberString.add(it.toString()) }
+    when (sortType) {
+        "byCount" -> sortedByCount(numberString,"numbers", numberString.size)
+        "natural" -> display(numbers.sorted().joinToString(" "), numbers.size, "numbers")
+    }
 }
-fun long() {
-    val numbers = mutableListOf<Int>()
-    while (scanner.hasNext()) numbers.add(scanner.nextInt())
-    val max: Int = numbers.maxOrNull() ?: 0
-    var noOfTimes = 0
-    for (i in numbers) if (i == max) noOfTimes++
-    result("numbers", "greatest number",numbers.size, max.toString(), noOfTimes)
-}
-fun line() {
-    val lines = mutableListOf<String>()
-    var max = ""
-    var noOfTimes = 0
-    while (scanner.hasNextLine()) lines.add(scanner.nextLine())
-    lines.forEach { if(it.length > max.length) max = it }
-    lines.forEach { if (it == max) noOfTimes++ }
-    result("lines", "longest line",lines.size, max, noOfTimes)
-}
-fun word() {
+fun word(sortType: String = "natural") {
     val words = mutableListOf<String>()
     while (scanner.hasNext()) words.add(scanner.next())
-    var max = ""
-    var noOfTimes = 0
-    words.forEach { if(it.length > max.length) max = it }
-    words.forEach { if (it == max) noOfTimes++ }
-    result("words", "longest word",words.size, max, noOfTimes)
+    when (sortType) {
+        "byCount" -> sortedByCount (words.sorted().toMutableList(), "words", words.size)
+        "natural" -> display(words.sorted().joinToString(" "), words.size, "words")
+    }
 }
-fun result(total: String, longest: String, size: Int, max: String, noOfTimes: Int) {
-    val percentage: Double = noOfTimes.toDouble() / size * 100.0
-    val newL = if (total == "lines") "\n" else " "
+fun line(sortType: String = "natural") {
+    val lines = mutableListOf<String>()
+    while (scanner.hasNextLine()) lines.add(scanner.nextLine())
+    when (sortType) {
+        "byCount" -> sortedByCount(lines.sorted().toMutableList(), "lines", lines.size)
+        "natural" -> display(lines.sorted().joinToString("\n"), lines.size, "lines")
+    }
+}
+fun sortedByCount(keys: MutableList<String>, total: String, size: Int) {
+    val countMap = keys.groupingBy { it }.eachCount()
+    val sortedMap = countMap.toList().sortedBy { (_, value) -> value}.toMap().toMutableMap()
     println("Total $total: $size.")
-    println("The $longest:$newL$max$newL($noOfTimes time(s),${percentage.toInt()}%).")
-    exitProcess(0)
+    for ((k, v) in sortedMap) print("$k: $v time(s), ${(v * 100.0 / size).roundToInt()}%\n")
 }
+fun display(sortedData: String, size: Int, dataType: String) {
+    println("Total $dataType: $size.")
+    print("Sorted data: ")
+    if (dataType != "lines")  println(sortedData) else println("\n$sortedData")
+}
+
