9842603bfa942589a6c5b402f6a95fb05ac9c679
Dagim Tesfaye <dagcodes@gmail.com>
2022-08-03 22:12:11 +0300

    Stage 6 complete. Project complete. Added file input and output.

--- Sorting Tool/task/src/sorting/Main.kt
+++ Sorting Tool/task/src/sorting/Main.kt
@@ -3,9 +3,10 @@ package sorting
 import java.util.Scanner
 import kotlin.math.roundToInt
 import kotlin.system.exitProcess
+import java.io.File
 
 val scanner  = Scanner(System.`in`)
-val commands =  listOf("-dataType", "-sortingType")
+val commands =  listOf("-dataType", "-sortingType", "-inputFile", "-outputFile")
 val dataTypes = listOf("long", "word", "line")
 val sortTypes = listOf("natural", "byCount")
 val allConfigurations = commands + dataTypes + sortTypes
@@ -13,14 +14,28 @@ val allConfigurations = commands + dataTypes + sortTypes
 fun main(args: Array<String>) {
     var typeOfData = try { args[args.indexOf(commands[0]) + 1] } catch (e: ArrayIndexOutOfBoundsException) {""}
     var typeOfSort = try { args[args.indexOf(commands[1]) + 1] } catch (e: ArrayIndexOutOfBoundsException) {""}
+    val inputFileName = if (args.contains(commands[2])) {
+        try {
+            args[args.indexOf(commands[2]) + 1]
+        } catch (e: Exception) {
+            ""
+        }
+    } else ""
+    val outputFileName = if (args.contains(commands[3])) {
+        try {
+            args[args.indexOf(commands[3]) + 1]
+        } catch (e: Exception) {
+            ""
+        }
+    } else ""
     checkArgs(args, typeOfData, typeOfSort)
     if (!dataTypes.contains(typeOfData)) typeOfData = dataTypes[1]
     if (!sortTypes.contains(typeOfSort)) typeOfSort = sortTypes[0]
+
     when (typeOfData) {
-        dataTypes[0] -> long(typeOfSort)
-        dataTypes[1] -> word(typeOfSort)
-        dataTypes[2] -> line(typeOfSort)
-        
+        dataTypes[0] -> long(typeOfSort,inputFileName, outputFileName)
+        dataTypes[1] -> word(typeOfSort,inputFileName, outputFileName)
+        dataTypes[2] -> line(typeOfSort,inputFileName, outputFileName)
     }
 }
 fun checkArgs(args: Array<String>, typeOfData: String, typeOfSort: String) {
@@ -36,10 +51,11 @@ fun checkArgs(args: Array<String>, typeOfData: String, typeOfSort: String) {
         if (!allConfigurations.contains(arg)) println("\"$arg\" is not a valid parameter. It will be skipped.")
     }
 }
-fun long(sortType: String = sortTypes[0]) {
+fun long(sortType: String = sortTypes[0], inputFileName: String, outputFileName: String) {
     var numbers = mutableListOf<Int>()
-    val numberString = mutableListOf<String>()
-    while (scanner.hasNext()) numberString.add(scanner.next())
+    var numberString = mutableListOf<String>()
+    if (inputFileName == "") while (scanner.hasNext()) numberString.add(scanner.next())
+    else numberString = File(inputFileName).readText().split(Regex("\\s+")).toMutableList()
     for (input in numberString) {
         try {
             input.toInt()
@@ -52,35 +68,48 @@ fun long(sortType: String = sortTypes[0]) {
     numbers = numbers.sorted().toMutableList()
     numbers.forEach { numberString.add(it.toString()) }
     when (sortType) {
-        sortTypes[1] -> sortedByCount(numberString,"numbers", numberString.size)
-        sortTypes[0] -> display(numbers.sorted().joinToString(" "), numbers.size, "numbers")
+        sortTypes[1] -> sortedByCount(numberString,"numbers", numberString.size, outputFileName)
+        sortTypes[0] -> display(numbers.sorted().joinToString(" "), numbers.size, "numbers", outputFileName)
     }
 }
-fun word(sortType: String = sortTypes[0]) {
-    val words = mutableListOf<String>()
-    while (scanner.hasNext()) words.add(scanner.next())
+fun word(sortType: String = sortTypes[0], inputFileName: String, outputFileName: String) {
+    var words = mutableListOf<String>()
+    if (inputFileName == "") while (scanner.hasNext()) words.add(scanner.next())
+    else words = File(inputFileName).readText().split(Regex("\\s+")).toMutableList()
     when (sortType) {
-        sortTypes[1] -> sortedByCount (words.sorted().toMutableList(), "words", words.size)
-        sortTypes[0] -> display(words.sorted().joinToString(" "), words.size, "words")
+        sortTypes[1] -> sortedByCount (words.sorted().toMutableList(), "words", words.size, outputFileName)
+        sortTypes[0] -> display(words.sorted().joinToString(" "), words.size, "words", outputFileName)
     }
 }
-fun line(sortType: String = sortTypes[0]) {
-    val lines = mutableListOf<String>()
-    while (scanner.hasNextLine()) lines.add(scanner.nextLine())
+fun line(sortType: String = sortTypes[0], inputFileName: String, outputFileName: String) {
+    var lines = mutableListOf<String>()
+    if (inputFileName == "") while (scanner.hasNextLine()) lines.add(scanner.nextLine())
+    else lines = File(inputFileName).readLines().toMutableList()
     when (sortType) {
-        sortTypes[1] -> sortedByCount(lines.sorted().toMutableList(), "lines", lines.size)
-        sortTypes[0] -> display(lines.sorted().joinToString("\n"), lines.size, "lines")
+        sortTypes[1] -> sortedByCount(lines.sorted().toMutableList(), "lines", lines.size, outputFileName)
+        sortTypes[0] -> display(lines.sorted().joinToString("\n"), lines.size, "lines", outputFileName)
     }
 }
-fun sortedByCount(keys: MutableList<String>, total: String, size: Int) {
+fun sortedByCount(keys: MutableList<String>, total: String, size: Int, outputFileName: String) {
     val countMap = keys.groupingBy { it }.eachCount()
     val sortedMap = countMap.toList().sortedBy { (_, value) -> value}.toMap().toMutableMap()
     println("Total $total: $size.")
     for ((k, v) in sortedMap) print("$k: $v time(s), ${(v * 100.0 / size).roundToInt()}%\n")
+    if (outputFileName != ""){
+        val outputFile = File(outputFileName)
+        outputFile.writeText("Total $total: $size.")
+        for ((k, v) in sortedMap) outputFile.writeText("$k: $v time(s), ${(v * 100.0 / size).roundToInt()}%\n")
+    }
 }
-fun display(sortedData: String, size: Int, dataType: String) {
+fun display(sortedData: String, size: Int, dataType: String, outputFileName: String) {
     println("Total $dataType: $size.")
     print("Sorted data: ")
     if (dataType != "lines")  println(sortedData) else println("\n$sortedData")
+    if (outputFileName != ""){
+        val outputFile = File(outputFileName)
+        outputFile.writeText("Total $dataType: $size.")
+        outputFile.writeText("Sorted data: ")
+        outputFile.writeText(if (dataType != "lines") sortedData else "\n$sortedData")
+    }
 }
 
